70551985072c32002dce69c38cdcee33b16b226f
Amom Ao Cubo <102225054+AmomDeAbreu@users.noreply.github.com>
2024-02-20 12:27:27 -0300

    it works a little

--- script.js
+++ script.js
@@ -4,26 +4,31 @@ const other_div = document.querySelector("#outro")
 let txt_input = document.querySelector("#texto");
 
 
-add_btn.addEventListener("click",() =>{
-    if(txt_input.value != ""){
-        const novo_nome = document.createElement("div");
-        novo_nome.setAttribute("class", "nome m")
-        novo_nome.innerHTML = txt_input.value;
-        other_div.appendChild(novo_nome);
-    }
-})
-
 const nomes = [...document.querySelectorAll(".nome")];
-
 nomes.map((el) => {
     el.addEventListener("click", () => {
         el.classList.toggle("selecionado");
     })
 })
 
-const selecionados = [...document.querySelectorAll(".selecionado")];
+
+add_btn.addEventListener("click",() =>{
+    if(txt_input.value != ""){
+        const novo_nome = document.createElement("div");
+        novo_nome.setAttribute("class", "nome")
+        novo_nome.innerHTML = txt_input.value;
+        other_div.appendChild(novo_nome);
+    }
+})
+
+
+
+
+
+
 rmv_btn.addEventListener("click", () => {
-    selecionados.map((el) => {
-        other_div.removeChild(el);
-    });
+    const selecionados = [...document.querySelectorAll(".selecionado")];
+    selecionados.map((e) => {
+        other_div.removeChild(e);
+    })
 })
