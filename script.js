04cf5e208e678f2a063f3b197837135ea265f4b6
Amom Ao Cubo <102225054+AmomDeAbreu@users.noreply.github.com>
2024-02-23 15:21:39 -0300

    Now you can add a name between another selected name! :)

--- script.js
+++ script.js
@@ -10,9 +10,12 @@ let txt_input = document.querySelector("#texto");
 const btn_slt = document.querySelector("#nm");
 /** Div principal (primeira) */
 const div_matrix = document.querySelector("#matriz");
+let is_selected = false;
+
 
 /** Divs que estão com a classe nome */
 const nomes = [...document.querySelectorAll(".nome")];
+//Mapeia os nomes que já estão presentes nas divs
 nomes.map((el) => {
     el.addEventListener("click", () => {
         el.classList.toggle("selecionado");
@@ -20,24 +23,49 @@ nomes.map((el) => {
 })
 
 
+
 // Adiciona nomes e permite que ao clicar nos nomes adicionados eles recebam a classe de selecionado;
 add_btn.addEventListener("click",() =>{
+    
+    nomes.map((el) => {
+        if(el.classList.contains("selecionado")){
+            is_selected = true;
+        }
+    })
+
     if(txt_input.value != "" && other_div.children.length <= 7 && txt_input.value.length < 19){
         /** instância de nova div com nome digitado */
         const novo_nome = document.createElement("div");
         novo_nome.setAttribute("class", "nome")
         novo_nome.innerText = txt_input.value;
+       // É necessário um toggle específico para os nomes que estão sendo adicionados
         novo_nome.addEventListener("click", () => {
             novo_nome.classList.toggle("selecionado");
         })
 
-        other_div.appendChild(novo_nome);
-    }
+        nomes.map((el) => {
+            if(el.classList.contains("selecionado")){
+                is_selected = true;
+            }
+        })
+        if(is_selected == false){
+            other_div.appendChild(novo_nome);
+        } else {
+            nomes.map((el) => {
+                if(el.classList.contains("selecionado")){
+                    const nome_selecionado = el;
+                    nome_selecionado.parentNode.insertBefore(novo_nome, nome_selecionado.nextSibling);
+                }
+            })
+        }
+    } 
 })
+
 
 
 // Remove nomes com a classe selecionados se for clicado em remover
 rmv_btn.addEventListener("click", () => {
+    /** Variável local para pegar valores selecionados */
     const selecionados = [...document.querySelectorAll(".selecionado")];
     selecionados.map((e) => {
         other_div.removeChild(e);
@@ -50,8 +78,9 @@ btn_slt.addEventListener("click", () => {
     if(div_matrix.children.length < 12){
         const todos_nomes_plmds = [...document.querySelectorAll(".nome")];
         let todos_selecionados = todos_nomes_plmds.filter((val) => {
-            if(val.classList.contains("selecionado"))
+            if(val.classList.contains("selecionado")){
                 return val;
+            }
         })
         todos_selecionados.map((el) =>{
             const p = document.createElement("p");
